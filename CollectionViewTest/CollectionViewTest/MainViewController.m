a5f357d919f0ef85ea6cc9e19b2d52c497d41baf
Patrick <patrick@helmsdeep.at>
2013-10-20 10:51:14 +0200

    Now the UICollectionView works
    For more details see:
    http://stackoverflow.com/questions/19453796/uicollectionviewcontroller-n
    ested-in-a-normal-uiviewcontroller

--- CollectionViewTest/CollectionViewTest/MainViewController.m
+++ CollectionViewTest/CollectionViewTest/MainViewController.m
@@ -7,11 +7,10 @@
 //
 
 #import "MainViewController.h"
-#import "CollectionViewController.h"
 
 @interface MainViewController ()
 
-@property (nonatomic, strong) CollectionViewController *collectionViewController;
+@property NSArray *collectionitems;
 
 @end
 
@@ -21,10 +20,7 @@
 {
     [super viewDidLoad];
     
-    _collectionViewController = [[CollectionViewController alloc] initWithCollectionViewLayout:_collectionView.collectionViewLayout];
-    
-    _collectionView.dataSource = _collectionViewController;
-    _collectionView.delegate = _collectionViewController;
+    _collectionitems = @[@"A", @"B", @"C", @"D", @"E", @"F", @"G", @"H", @"I", @"J", @"K", @"L", @"M", @"N", @"O", @"P", @"Q", @"R", @"S", @"T", @"U", @"V", @"W", @"X", @"Y", @"Z"];
 }
 
 - (void)didReceiveMemoryWarning
@@ -34,6 +30,51 @@
 }
 
 - (IBAction)testButtonPressed:(id)sender {
-    [_collectionViewController outputSelectedCell];
+    NSArray *indexPaths = [self.collectionView indexPathsForSelectedItems];
+    NSIndexPath *indexPath = [indexPaths objectAtIndex:0];
+    
+    NSString *itemValue = _collectionitems[indexPath.row];
+    
+    if ([indexPaths count] > 0) {
+        NSLog(@"Selected item: %@", itemValue);
+    } else {
+        NSLog(@"Nothing selected.");
+    }
+}
+
+#pragma mark - collection view stuff
+
+- (NSInteger)collectionView:(UICollectionView *)view numberOfItemsInSection:(NSInteger)section
+{
+    return [_collectionitems count];
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"customCell" forIndexPath:indexPath];
+    
+    return cell;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didHighlightItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *stageButton = [collectionView cellForItemAtIndexPath:indexPath];
+    stageButton.backgroundColor = [UIColor lightGrayColor];
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didUnhighlightItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *stageButton = [collectionView cellForItemAtIndexPath:indexPath];
+    stageButton.backgroundColor = [UIColor greenColor];
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"Selected item: %i", indexPath.row);
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didDeselectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"Unselected item: %i", indexPath.row);
 }
 @end
